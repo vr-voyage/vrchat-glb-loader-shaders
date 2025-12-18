61bc137608ea4f8922daff96f41cd75601d81ca9
Voyage <voyage@miouyouyou.fr>
2025-12-18 23:59:52 +0100

    Separating the shaders
    So, I need to write some utilities for the
    3D Model Loader.
    These utilities don't require the VRChat package,
    but require these shaders to work.
    
    I could put everything in one single package, but
    then the people who only want the gimmick inside
    their worlds would have to install a whole bunch
    of packages required by the utilities, even if they
    don't need these utilities.
    
    So the shaders are now present in a separate package.
    
    Signed-off-by: Voyage <voyage@miouyouyou.fr>

---
+++ README.md
@@ -0,0 +1,16 @@
+# About
+
+This repository contains the shaders used by my
+[https://github.com/vr-voyage/vrchat-glb-loader](VRChat 3D Model Loader), and its
+utilities.
+
+Since the utilities can be installed on separate project,
+the shaders are separated from the main package.
+
+These shaders are just modified versions of :
+* [MToon 1.x](https://github.com/vrm-c/UniVRM/tree/master/Packages/VRM10/MToon10)
+* [ShaderMotion](https://github.com/CuteWaterBeary/ShaderMotion)
+
+Their namespaces and names were slightly change to avoid conflicting
+with the official versions of this shader when present in the same
+Unity project.
---
+++ Runtime/ShaderMotion/vrmc_materials_mtoon_attribute.hlsl
@@ -0,0 +1,40 @@
+﻿#ifndef VRMC_MATERIALS_MTOON_ATTRIBUTE_INCLUDED
+#define VRMC_MATERIALS_MTOON_ATTRIBUTE_INCLUDED
+
+#include "./vrmc_materials_mtoon_render_pipeline.hlsl"
+
+struct Attributes
+{
+    float3 vertex : POSITION; // UnityCG macro specified name. Accurately "positionOS"
+    float3 normalOS : NORMAL;
+    float4 tangentOS : TANGENT;
+    float4 texcoord0 : TEXCOORD0;
+    float4 boneWeights : TEXCOORD1;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+struct Varyings
+{
+    float2 uv : TEXCOORD0;
+    float3 vertex : TEXCOORD1;
+    half3 normalWS : TEXCOORD2;
+    float3 positionWS : TEXCOORD3;
+#if defined(_NORMALMAP)
+    half4 tangentWS : TEXCOORD4;
+#endif
+    float3 viewDirWS : TEXCOORD5;
+
+    MTOON_FOG_AND_LIGHTING_COORDS(5, 6, 7)
+    
+    float4 pos : SV_POSITION; // UnityCG macro specified name. Accurately "positionCS"
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+    UNITY_VERTEX_OUTPUT_STEREO
+};
+
+struct FragmentInput
+{
+    Varyings varyings;
+    MTOON_FRONT_FACE_TYPE facing : MTOON_FRONT_FACE_SEMANTIC;
+};
+
+#endif
